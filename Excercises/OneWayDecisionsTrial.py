5429ddd3205af0a9f78a0996961fae7a4f4ef2e2
Lucas <kyuoven@duck.com>
2022-09-15 16:27:36 +0200

    Added Folders for exercises and quizzes

---
+++ Excercises/OneWayDecisionsTrial.py
@@ -0,0 +1,15 @@
+x = 5
+print("Lets see what numbers I have in my pocket!")
+print("I count one........I thought I had two numbers??????????")
+if x == 5:
+    print("Our number is 5!")
+    print("Yep, that's definitively a 5.")
+    print("I am pretttyyyy sure that's a 5...")
+    print("Okay now I am doubting, let me see!")
+y = x + 2
+print("Oh I found another one!")
+print("I have two numbers in my pocket!")
+print("I have a", x, "and a", y)
+if y == 7:
+    print("I knew there was another number!")
+print("Let's see if I can get another number out of my pocket!")
---
+++ Quizzes/Quizzes_trial.py
@@ -0,0 +1,9 @@
+x = 7
+x = x + 2
+print(x)
+
+a = 42 % 10
+print(a)
+
+b = int(98.6)
+print(b)
